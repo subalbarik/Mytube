a5d48530d775374cafeb3d9620439e414cb1b83a
subal <subalbarik7780@gmail.com>
2025-07-05 22:31:15 +0530

    update endpoint of account deatils and avatar & coverimage

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -292,6 +292,65 @@ const updateAccountDetails = asyncHandler(async(req,res)=>{
 
 })
 
+const updateUserAvatar = asyncHandler(async(req,res)=>{
+   const avatarLocalpath = req.file?.path
+
+   if (!avatarLocalpath) {
+      throw new ApiError(400,"Avatar file is required")
+   }
+
+   const avatar = await uploadOnCloudinary(avatarLocalpath)
+
+   if (!avatar.url) {
+      throw new ApiError(400,"Error while uploading avatar")
+   }
+
+   const user = await User.findByIdAndUpdate(
+      req.user?._id,
+      {
+         $set:{
+            avatar:avatar.url
+         }
+      },
+      {new:true}
+   ).select("-password")
+    
+   return res
+   .status(200)
+   .json(new ApiResponse(200,user,"Avatar iamge updated successfully"))
+})
+
+const updateUserCoverImage = asyncHandler(async(req,res)=>{
+   const coverImageLocalPath= req.file?.path
+
+   if (!coverImageLocalPath) {
+      throw new ApiError(400,"cover image is required")
+   }
+
+   const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+   if (!coverImage.url) {
+      throw new ApiError(400,"Error while uploading coverImage")
+   }
+
+   const user = await User.findByIdAndUpdate(
+      req.user?._id,
+      {
+         $set:{
+            coverImage:coverImage.url
+         }
+      },
+      {new:true}
+   ).select("-password")
+
+   return res
+   .status(200)
+   .json(
+      new ApiResponse(200,user,"coverImage updated successfully")
+   )
+
+})
+
 export {
    registerUser,
    loginUser,
@@ -299,5 +358,7 @@ export {
    refreshAccessToken,
    changeCurrentPassword,
    getCurrentUser,
-   updateAccountDetails
+   updateAccountDetails,
+   updateUserAvatar,
+   updateUserCoverImage
 } 
